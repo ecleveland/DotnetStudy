6297f439b9ab031c31cca78b4086d7422eebf7b6
Eric Cleveland <eric.cleveland9@live.com>
2024-10-25 11:11:50 -0600

    Modified to do first theory unit tests

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -2,14 +2,23 @@ namespace PrimeService.Tests;
 
 public class PrimeService_IsPrimeShould
 {
-    [Fact]
-    public void IsPrime_Input1_ReturnFalse()
+    private readonly PrimeService _primeService;
+
+    public PrimeService_IsPrimeShould()
+    {
+        _primeService = new PrimeService();
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void IsPrime_Input1_ReturnFalse(int value)
     {
         // Arrange
-        var primeService = new PrimeService();
 
         // Act
-        bool result = primeService.IsPrime(1);
+        bool result = _primeService.IsPrime(value);
 
         // Assert
         Assert.False(result, "1 should not be prime");
--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -4,9 +4,9 @@ public class PrimeService
 {
     public bool IsPrime(int candidate)
     {
-        if(candidate == 1)
+        if(candidate < 2)
         {
-            return true;
+            return false;
         }
         throw new NotImplementedException("Not fully implemented.");
     }
